a6d85e2702f48900fd68af7f8130a1cac29c48f7
RiteshChawlaAlivelyC <riteshchawla.anjalichawla@gmail.com>
2025-01-04 19:00:49 -0800

    Initial Commit

---
+++ src/main/java/com/iceberg/tables/creator/application/IcebergTablesCreatorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.iceberg.tables.creator.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcebergTablesCreatorMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcebergTablesCreatorMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iceberg/tables/creator/application/IcebergTablesCreatorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iceberg.tables.creator.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcebergTablesCreatorMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
